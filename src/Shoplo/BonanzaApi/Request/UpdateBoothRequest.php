ba4aa19fc10bc096f6538a742a1ec4655b0de72f
marrek13 <marrek13@gmail.com>
2017-09-19 15:57:54 +0200

    Missing import

--- src/Shoplo/BonanzaApi/Request/UpdateBoothRequest.php
+++ src/Shoplo/BonanzaApi/Request/UpdateBoothRequest.php
@@ -4,13 +4,13 @@
 namespace Shoplo\BonanzaApi\Request;
 
 
+use JMS\Serializer\Annotation as Serializer;
 use Shoplo\BonanzaApi\Type\RequesterCredentialsType;
 
 class UpdateBoothRequest
 {
 	/**
 	 * @var RequesterCredentialsType
-	 *
 	 * @Serializer\Type("Shoplo\BonanzaApi\Type\RequesterCredentialsType")
 	 */
 	public $requesterCredentials;
