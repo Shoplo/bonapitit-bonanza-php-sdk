e5443c9bcc29322636ad847010d747dcf2135c5c
Paul P <bigpaulie25ro@yahoo.com>
2018-02-08 09:05:26 +0200

    Changing serialization type for categoryId to string rather than int in order to facilitate usage of the "Guess" category feature. Reformatting the code.

--- src/Shoplo/BonanzaApi/Type/FixedPriceItemResponseType.php
+++ src/Shoplo/BonanzaApi/Type/FixedPriceItemResponseType.php
@@ -8,31 +8,31 @@ use JMS\Serializer\Annotation as Serializer;
 
 class FixedPriceItemResponseType extends BaseResponseType
 {
-	/**
-	 * @var string
-	 *
-	 * @Serializer\Type("string")
-	 */
-	public $sellingState;
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     */
+    public $sellingState;
 
-	/**
-	 * @var int
-	 *
-	 * @Serializer\Type("int")
-	 */
-	public $categoryId;
+    /**
+     * @var int
+     *
+     * @Serializer\Type("int")
+     */
+    public $categoryId;
 
-	/**
-	 * @var int
-	 *
-	 * @Serializer\Type("int")
-	 */
-	public $itemId;
+    /**
+     * @var int
+     *
+     * @Serializer\Type("int")
+     */
+    public $itemId;
 
     /**
      * @var string
      *
      * @Serializer\Type("string")
      */
-	public $message;
+    public $message;
 }
--- src/Shoplo/BonanzaApi/Type/PrimaryCategoryType.php
+++ src/Shoplo/BonanzaApi/Type/PrimaryCategoryType.php
@@ -11,7 +11,7 @@ class PrimaryCategoryType
 	/**
 	 * @var int
 	 *
-	 * @Serializer\Type("int")
+	 * @Serializer\Type("string")
 	 */
 	public $categoryId;
 
